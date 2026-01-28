b935cf5ec96df5923cb279896eea004e02ace27e
Egor Suncheleev <suncheleev.egor@mail.ru>
2026-01-28 18:41:19 +0400

    Кнопка

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__burger-menu-nav');
+
+burger.addEventListener('click', function(e) {
+  e.stopPropagation();
+  menu.classList.toggle('active')
+});
 
