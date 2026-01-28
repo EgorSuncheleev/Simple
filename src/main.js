148d85518c95498b024be9ef0afc7181f6073bef
Egor Suncheleev <suncheleev.egor@mail.ru>
2026-01-28 20:37:16 +0400

    Исправил все

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.header__burger-menu-nav');
+let del = document.querySelector('.menu-delete')
 
 burger.addEventListener('click', function(e) {
   e.stopPropagation();
   menu.classList.toggle('active')
 });
 
+del.addEventListener('click', function(e) {
+  e.stopPropagation();
+  menu.classList.remove('active')
+})
