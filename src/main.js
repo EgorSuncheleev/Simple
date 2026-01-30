0691edf2a299b96f3fd517a140d74d1ef2df87f1
Egor Suncheleev <suncheleev.egor@mail.ru>
2026-01-30 22:46:07 +0400

    Нfarg

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.header__burger-menu-nav');
 let del = document.querySelector('.menu-delete')
+let anchor = document.querySelector('.js-anchor')
 
 burger.addEventListener('click', function(e) {
   e.stopPropagation();
@@ -11,3 +12,15 @@ del.addEventListener('click', function(e) {
   e.stopPropagation();
   menu.classList.remove('active')
 })
+
+anchor.addEventListener('click', function(e) {
+  e.stopPropagation();
+  menu.classList.remove('active')
+
+  setTimeout(() => {
+      anchor.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  })
+  }, 1500)
+});
